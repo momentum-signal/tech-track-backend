73d8a186b18a9455ac75b1f06b887e69fc2c77a3
nimjoe <nimjoe128@gmail.com>
2024-12-17 13:56:36 +0800

    I just create a temp function

--- src/main/java/com/techtrack/backend/Application/model/ApplicationModel.java
+++ src/main/java/com/techtrack/backend/Application/model/ApplicationModel.java
@@ -1,5 +1,7 @@
 package com.techtrack.backend.Application.model;
 
 public class ApplicationModel {
-    
+    public void temp(String[] args){
+        System.out.println("InternshipModel");
+    }
 }
